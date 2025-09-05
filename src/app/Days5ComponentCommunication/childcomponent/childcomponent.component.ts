8f26207678a266b59d36bb8ccd38dbccd902c784
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2025-09-05 13:01:10 +0500

    add child and parent components with initial templates and tests

---
+++ src/app/Days5ComponentCommunication/childcomponent/childcomponent.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-childcomponent',
+  imports: [],
+  templateUrl: './childcomponent.component.html',
+  styleUrl: './childcomponent.component.css'
+})
+export class ChildcomponentComponent {
+
+}
